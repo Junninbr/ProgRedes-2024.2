99968057433bae57cc3ea57261637b02b2277cea
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2024-12-30 18:38:47 -0300

    Add files via upload

--- Unidade01-Avaliacao02/Q1/server/udp-file-server-size.py
+++ Unidade01-Avaliacao02/Q1/server/udp-file-server-size.py
@@ -1,29 +1,40 @@
 import socket 
+import os  
 
 server= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip = "192.168.1.6" 
-porta= 65432 
+ip = "127.0.0.1" 
+porta= 1234
 server_address= (ip, porta)
+DIRETORIO= 'files/'
 
 server.bind(server_address) 
 
 print(f'Servidor aguardando solicitações em {ip}:{porta}')
-
-while True: 
+ 
+try: 
     filename, client_address = server.recvfrom(1024)
-    filename= filename.decode 
+    filename= filename.decode() 
 
-    if filename: 
-        try: 
-            with open (filename, 'rb') as file: 
-                file.seek(0, 2)
-                tam = file.tell()
-            print(f'Enviando o tamanho do arquivo {filename } ({tam} bytes) ao cliente {client_address}')
+    path= os.path.join(DIRETORIO, filename)
+    if os.path.isfile(path):
+        tam=0
+        with open(path, 'rb') as file:
+            while True: 
+                bloco = file.read(1024)
+                if not 1024:
+                        break 
+                tam += len(bloco)
+            print(f'Enviando o tamanho do arquivo {filename} ({tam} bytes ao cliente {client_address})')
             server.sendto(str(tam).encode(), client_address)
-        
-        except FileNotFoundError: 
-            print(f'O arquivo solicitado {filename} não foi encontrado no servidor {server_address}')
-            
     else: 
-        print(f'Nenhum arquivo foi solicitado pelo cliente {client_address}')
-        server.sendto(b'Nenhum arquivo especificado!', client_address)
+         print(f'O arquivo {filename} não foi encontrado no diretório {DIRETORIO}')
+         server.sendto(b'0', client_address)
+
+except FileNotFoundError: 
+    print(f'O arquivo solicitado {filename} não foi encontrado no servidor {server_address}')
+            
+
+
+
+
+        
