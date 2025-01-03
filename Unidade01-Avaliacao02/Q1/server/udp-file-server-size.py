e09c9064216deecc0ad9b9b6e17982a41d9171ac
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2025-01-03 18:29:14 -0300

    udp-file-server-size.py
    Código final

--- Unidade01-Avaliacao02/Q1/server/udp-file-server-size.py
+++ Unidade01-Avaliacao02/Q1/server/udp-file-server-size.py
@@ -9,29 +9,35 @@ DIRETORIO= 'files/'
 
 server.bind(server_address) 
 
-print(f'Servidor aguardando solicitações em {ip}:{porta}')
+print(f'Servidor escutando em.... {ip}:{porta}')
  
-try: 
-    filename, client_address = server.recvfrom(1024)
-    filename= filename.decode() 
+try:
+    while True:
+        # Recebe o nome do arquivo requisitado
+        filename, client_address = server.recvfrom(2048) # Buffer de até 2048 bytes
+        filename = filename.decode() 
+        path = os.path.join(DIRETORIO, filename)
+        
+        # Verifica se o arquivo existe no diretório
+        if os.path.isfile(path):
+            tam = os.path.getsize(path) # Consegue o tamanho do arquivo 
+            print(f'Enviando o tamanho do arquivo {filename} ({tam} bytes) ao cliente {client_address}') 
+            server.sendto(str(tam).encode(), client_address) # Envio do tamanho do arquivo como string codificada em bytes ao cliente 
 
-    path= os.path.join(DIRETORIO, filename)
-    if os.path.isfile(path):
-        tam=0
-        with open(path, 'rb') as file:
-            while True: 
-                bloco = file.read(1024)
-                if not 1024:
-                        break 
-                tam += len(bloco)
-            print(f'Enviando o tamanho do arquivo {filename} ({tam} bytes ao cliente {client_address})')
-            server.sendto(str(tam).encode(), client_address)
-    else: 
-         print(f'O arquivo {filename} não foi encontrado no diretório {DIRETORIO}')
-         server.sendto(b'0', client_address)
+        # Leitura do arquivo e o envio dos dados em blocos de 2048 bytes
+            with open(path, 'rb') as file:
+                while chunk := file.read(2048): # Enquanto o arquivo não tiver chegado ao final, leia o próximo bloco de 2048 bytes
+                    server.sendto(chunk, client_address)
+            print(f'Arquivo {filename} enviado com sucesso para {client_address}') # Envia o bloco ao cliente
+        else:
+            # Caso o arquivo não for encontrado ele enia '0' ao cliente
+            print(f'O arquivo {filename} não foi encontrado no diretório {DIRETORIO}')
+            server.sendto(b'0', client_address)
 
-except FileNotFoundError: 
-    print(f'O arquivo solicitado {filename} não foi encontrado no servidor {server_address}')
+except Exception as e:
+    print(f"Erro no servidor: {e}")
+finally:
+    server.close()
             
 
 
