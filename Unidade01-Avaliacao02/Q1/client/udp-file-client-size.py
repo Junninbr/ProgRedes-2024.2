3aa12727b3d85f6c7a978e6c26ed71603a48e2a0
Arnaldo Junior <143155880+Junninbr@users.noreply.github.com>
2025-01-03 18:41:02 -0300

    Código "final"

--- Unidade01-Avaliacao02/Q1/client/udp-file-client-size.py
+++ Unidade01-Avaliacao02/Q1/client/udp-file-client-size.py
@@ -1,36 +1,43 @@
 import socket
 
-SERVER = '192.168.1.6'
-PORT = 3456
+SERVER = '127.0.0.1' 
+PORT = 1234 
 DIRETORIO = "files/"
 
-udpSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udpSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Varíavel que contém os Protocolos IPv4 e UDP.
 
+# Loop para iterar o pedido do arquivo. 
 while True:
-    nomeArq = input("Digite o nome do arquivo desejado: ")
-
-    print ("Enviando pedido a", (SERVER, PORT), "para", nomeArq)
-    udpSock.sendto(nomeArq.encode('utf-8'), (SERVER, PORT))
-
-    dataTam, source = udpSock.recvfrom(1024)
-    try:
-        tamArq = int(dataTam.decode('utf-8'))
-    except ValueError:
-        print("Erro: Não foi possível receber o tamanho do arquivo ou não foi encontrado.")
-        continue
-
-    if tamArq > 0:
-        print("Salvando o arquivo localmente...")
-        with open(DIRETORIO+nomeArq, "wb") as fd:
-            recebido = 0
-            while recebido < tamArq:
-                data, source = udpSock.recvfrom(4096)
-                fd.write(data)
-                print("lidos: ", len(data))
-                recebido += len(data)
-            print(f"O arquivo {nomeArq} foi recebido com sucesso")
-    
+    nomeArq = input("Digite o nome do arquivo desejado ou digite 0 para sair: ") # Input para receber o nome do arquivo.
+    if nomeArq == "0": # Caso seja digitado 0, a conexão com o servidor é fechado e o loop é encerrado.
+        udpSock.close()
+        print("Encerrando programa...")
+        break
     else:
-        print("Erro: Arquivo desejado não existe ou não foi encotrado.")
+        print ("Enviando pedido a", (SERVER, PORT), "para", nomeArq) # Caso seja pedido um arquivo, será printado o nome em si ao servidor conectado.
+        udpSock.sendto(nomeArq.encode('utf-8'), (SERVER, PORT)) # Enviará o pedido codificado em UTF-8.
 
-udpSock.close
+        dataTam, source = udpSock.recvfrom(2048) # Pacote contendo o tamanho do arquivo solicitado.
+
+        try:
+            tamArq = int(dataTam.decode('utf-8')) # Transforma o pacote contendo o tamanho em inteiro e printa o nome e tamanho.
+            print(f"O arquivo '{nomeArq}' possui o tamanho de {tamArq} Bytes.")
+        except ValueError: # Caso o valor recebido seja 0, dará essa exceção.
+            continue
+
+        if tamArq > 0: # Se o valor recebido for maior que 0, criará um arquivo vazio com o mesmo nome no diretório.
+            print("Salvando o arquivo localmente...")
+            with open(DIRETORIO+nomeArq, "wb") as fd:
+                recebido = 0 # varíavel para receber o tamanho do arquivo.
+                while recebido < tamArq:
+                    data, source = udpSock.recvfrom(4096) # Será recebido mais um bloco de bytes contendo o arquivo em si e será escrito até o loop ser encerrado.
+                    fd.write(data)
+                    print("lidos: ", len(data), "Bytes") # Printa quantos bytes foram recebidos e lidos.
+                    recebido += len(data)
+                print(f"O arquivo '{nomeArq}' foi recebido com sucesso")
+        
+        else:
+            print("Erro: Arquivo desejado não existe ou não foi encotrado.") # Caso o valor recebido seja 0, dará o erro.
+
+        udpSock.close() # A conexão com o servidor é fechado e o loop é encerrado.
+        break
