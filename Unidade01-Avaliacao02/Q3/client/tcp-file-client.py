747c4eca3aa5ca1348b15c0d98f0b49f0558de10
Arnaldo Junior <143155880+Junninbr@users.noreply.github.com>
2025-01-06 16:23:05 -0300

    Add files via upload

--- Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
+++ Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
@@ -9,7 +9,7 @@ tcpSock.connect((SERVER, PORT))
 
 # Loop para iterar o pedido do arquivo. 
 while True:
-    nomeArq = input("Digite o nome do arquivo desejado ou digite 0 para sair: ") # Input para receber o nome do arquivo.
+    nomeArq = input("Digite o comando desejado ou digite 0 para sair: ") # Input para receber o nome do arquivo.
     if nomeArq == "0": # Caso seja digitado 0, a conexão com o servidor é fechado e o loop é encerrado.
         tcpSock.close()
         print("Encerrando programa...")
@@ -25,28 +25,30 @@ while True:
         break
 
 
-    else:
-        print ("Enviando pedido a", (SERVER, PORT), "para", nomeArq) # Caso seja pedido um arquivo, será printado o nome em si ao servidor conectado.
-        tcpSock.send(nomeArq.encode('utf-8')) # Enviará o pedido codificado em UTF-8.
+    elif nomeArq == "sget":
+        arquivo = input("Digite o nome do arquivo solicitado: \n")
+
+        print ("Enviando pedido a", (SERVER, PORT), "para", arquivo) # Caso seja pedido um arquivo, será printado o nome em si ao servidor conectado.
+        tcpSock.send(arquivo.encode('utf-8')) # Enviará o pedido codificado em UTF-8.
 
         dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
 
         try:
             tamArq = int(dataTam.decode('utf-8')) # Transforma o pacote contendo o tamanho em inteiro e printa o nome e tamanho.
-            print(f"O arquivo '{nomeArq}' possui o tamanho de {tamArq} Bytes.")
+            print(f"O arquivo '{arquivo}' possui o tamanho de {tamArq} Bytes.")
         except ValueError: # Caso o valor recebido seja 0, dará essa exceção.
             continue
 
         if tamArq > 0: # Se o valor recebido for maior que 0, criará um arquivo vazio com o mesmo nome no diretório.
             print("Salvando o arquivo localmente...")
-            with open(DIRETORIO+nomeArq, "wb") as fd:
+            with open(DIRETORIO+arquivo, "wb") as fd:
                 recebido = 0 # varíavel para receber o tamanho do arquivo.
                 while recebido < tamArq:
                     data = tcpSock.recv(4096) # Será recebido mais um bloco de bytes contendo o arquivo em si e será escrito até o loop ser encerrado.
                     fd.write(data)
                     print("lidos: ", len(data), "Bytes") # Printa quantos bytes foram recebidos e lidos.
                     recebido += len(data)
-                print(f"O arquivo '{nomeArq}' foi recebido com sucesso")
+                print(f"O arquivo '{arquivo}' foi recebido com sucesso")
         
         else:
             print("Erro: Arquivo desejado não existe ou não foi encotrado.") # Caso o valor recebido seja 0, dará o erro.
