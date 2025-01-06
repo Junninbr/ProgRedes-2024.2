39d96f55a834e732f9dbd8092e721f825f9daa05
Arnaldo Junior <143155880+Junninbr@users.noreply.github.com>
2025-01-06 17:25:51 -0300

    Update tcp-file-client.py

--- Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
+++ Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 SERVER = '127.0.0.1' 
 PORT = 1234 
@@ -27,7 +28,7 @@ while True:
 
     elif nomeArq == "sget":
         pedido = "2"
-        print("Enviando pedido de download de um único arquivo.\n")
+        print("Enviando pedido de download de um único arquivo ao servidor...\n")
         tcpSock.send(pedido.encode('utf-8'))
         dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
         resposta = str(dataTam.decode('utf-8'))
@@ -61,3 +62,66 @@ while True:
 
         tcpSock.close() # A conexão com o servidor é fechado e o loop é encerrado.
         break
+
+    elif nomeArq == "mget":
+        pedido = "3"
+        print("Enviando pedido de download de múltiplos arquivos por máscara ao servidor...\n")
+        tcpSock.send(pedido.encode('utf-8'))
+        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+        resposta = str(dataTam.decode('utf-8'))
+        
+        
+        arquivo = input(resposta)
+        if arquivo[0] != "*":
+            print("Erro de sinxtaxe, digite um nome válido. Ex.: *.jpg, *.xml.")
+        else:
+            print("Enviando pedido a", (SERVER, PORT), "para todos os arquivos contendo", arquivo) # Caso seja pedido um arquivo, será printado o nome em si ao servidor conectado.
+            tcpSock.send(arquivo.encode('utf-8')) # Enviará o pedido codificado em UTF-8.
+
+            dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+            numArq = int(dataTam.decode('utf-8'))
+
+            for _ in numArq:
+                dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+                try:
+                    tamArq = int(dataTam.decode('utf-8')) # Transforma o pacote contendo o tamanho em inteiro e printa o nome e tamanho.
+                    print(f"O arquivo '{arquivo}' possui o tamanho de {tamArq} Bytes.")
+                except ValueError: # Caso o valor recebido seja 0, dará essa exceção.
+                    continue
+                
+                if os.path.exists(arquivo):
+                    resp = input(f"O arquivo {arquivo} já existe, deseja sobrescrever? S ou N: ")
+                    if resp == "S":
+                        if tamArq > 0: # Se o valor recebido for maior que 0, criará um arquivo vazio com o mesmo nome no diretório.
+                            print("Salvando o arquivo localmente...")
+                            with open(DIRETORIO+arquivo, "wb") as fd:
+                                recebido = 0 # varíavel para receber o tamanho do arquivo.
+                                while recebido < tamArq:
+                                    data = tcpSock.recv(4096) # Será recebido mais um bloco de bytes contendo o arquivo em si e será escrito até o loop ser encerrado.
+                                    fd.write(data)
+                                    print("lidos: ", len(data), "Bytes") # Printa quantos bytes foram recebidos e lidos.
+                                    recebido += len(data)
+                                print(f"O arquivo '{arquivo}' foi recebido com sucesso")
+                        else:
+                            print("Erro: Arquivo não encontrado.")
+                    else:
+                        print("Operação cancelada.")
+                        break
+                        
+                else:
+                    if tamArq > 0: # Se o valor recebido for maior que 0, criará um arquivo vazio com o mesmo nome no diretório.
+                        print("Salvando o arquivo localmente...")
+                        with open(DIRETORIO+arquivo, "wb") as fd:
+                            recebido = 0 # varíavel para receber o tamanho do arquivo.
+                            while recebido < tamArq:
+                                data = tcpSock.recv(4096) # Será recebido mais um bloco de bytes contendo o arquivo em si e será escrito até o loop ser encerrado.
+                                fd.write(data)
+                                print("lidos: ", len(data), "Bytes") # Printa quantos bytes foram recebidos e lidos.
+                                recebido += len(data)
+                            print(f"O arquivo '{arquivo}' foi recebido com sucesso")
+                    
+                    else:
+                        print("Erro: Arquivo não encontrado.")
+
+                tcpSock.close() # A conexão com o servidor é fechado e o loop é encerrado.
+                break
