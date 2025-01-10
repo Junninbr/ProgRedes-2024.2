204160d926c6b3869b2fc2ca75e067ff5419df55
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2025-01-10 19:53:43 -0300

    Add files via upload

--- Unidade01-Avaliacao02/Q3/server/tcp-file-server.py
+++ Unidade01-Avaliacao02/Q3/server/tcp-file-server.py
@@ -119,5 +119,53 @@ while True:
                             sha1 = (f'O hash SHA1 obtido do arquivo {name_arq} até a posição {pos} corresponde a : \n{calc} ')
                             client_socket.sendall(sha1.encode())
                             client_socket.sendall(b'Hash enviado com sucesso!')
+        # Caso o cliente desejar continuar o download de um arquivo do servidor a partir de onde foi interrompido
+        elif filename == "5":
+                print(f'Recibida a solicitação de continuar o download do arquivo do cliente {client_address} ') 
+                client_socket.sendall(b'Envie o nome do arquivo e o hash da parte baixada (exemplo: barco.jpg:hash): ')
+                novo_hash= client_socket.recv(2048).decode()
+                name_arq, hash= novo_hash.split(':') # Separação do nome do arquivo e do hash enviado pelo cliente
+                if not ":" in novo_hash:
+                    erro = client_socket.sendall(f'ERRO! O formato de escrita para o hash {novo_hash} está incorreto!'.encode())
+                    print(erro) 
+
+                elif name_arq not in DIRETORIO:
+                    erro = client_socket.sendall(f'ERRO! o arquivo {name_arq} não existe no diretório {DIRETORIO}'.encode())      
+                    print(erro)    
+                
+                elif ':' in novo_hash:
+                    path= os.path.join(DIRETORIO, name_arq)
+                    # Calculo do hash do arquivo na pasta files do SERVIDOR
+                    if os.path.isfile(path):
+                        with open (path, 'rb') as file:
+                            leitura = file.read()
+                            calc= hashlib.sha1(leitura).hexdigest()
+                            server_hash= calc
+                    # Comparando cada hash
+                    print(f'Hash do cliente: {novo_hash}')
+                    print(f'Hash do servidor: {server_hash}')
+            
+                    # Verificando os hashes. o arquivo é enviado quando ambos os hashes forem iguais
+
+                    if novo_hash == server_hash:
+                        client_socket.sendall(b'Os hashes do servidor e do cliente coincidem')
+                        client_socket.sendall(b'Enviando o restante do arquivo....')
+
+                        # Envio do restante do arquivo até a interrupção do cliente
+                        resto = leitura[len(novo_hash):] # Seleciona os bytes do arquivo que o cliente ainda não recebeu
+                        while resto: 
+                           chunk = resto[:2048] # Pega o primeiro bloco de 2048 bytes do restante do arquivo 
+                           client_socket.sendall(chunk)
+                           resto = resto [:2048] # Os próximos 2048 bytes
+                        client_socket.sendall(f'Restante do arquivo {name_arq} enviado com sucesso!'.encode('utf-8'))
+
+                    elif novo_hash != server_hash: 
+                        client_socket.sendall(f'ERRO! Os Hashes do servidor e do cliente do arquivo {name_arq} não são compatives!'.encode('utf-8'))
+                    else: 
+                        client_socket.sendall(f'ERRO! Falha no cálculo dos hashes do arquivo {name_arq}'.encode('utf-8'))
+
+
+
+
     
     client_socket.close()
