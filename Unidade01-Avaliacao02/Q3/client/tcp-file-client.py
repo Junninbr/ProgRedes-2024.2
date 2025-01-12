e9b0dd820f87c23f96da70ef084770ccb0731799
Arnaldo Junior <143155880+Junninbr@users.noreply.github.com>
2025-01-11 23:30:55 -0300

    Update tcp-file-client.py

--- Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
+++ Unidade01-Avaliacao02/Q3/client/tcp-file-client.py
@@ -13,7 +13,7 @@ tcpSock.connect((SERVER, PORT))
 global leitura
 
 def pedir_arquivo(arquivo, tamArq):
-    if os.path.exists(DIRETORIO + arquivo):  # Verifica se o arquivo já existe
+    if os.path.exists(DIRETORIO + arquivo):
         resp = input(f"O arquivo '{arquivo}' já existe, deseja sobrescrever? S ou N: ")
         if resp != "S":
             print("Operação cancelada.")
@@ -22,36 +22,35 @@ def pedir_arquivo(arquivo, tamArq):
             print(f"Substituindo o arquivo '{arquivo}'...")
             
     
-    if tamArq > 0:  # Se o tamanho do arquivo for maior que 0, criaremos e salvaremos o arquivo
+    if tamArq > 0:
         print(f"Salvando o arquivo '{arquivo}' localmente...")
         with open(DIRETORIO + arquivo, "wb") as fd:
             recebido = 0
             while recebido < tamArq:
-                data = tcpSock.recv(4096)  # Recebe dados em blocos
+                data = tcpSock.recv(4096)
                 fd.write(data)
-                print("Lidos: ", len(data), "Bytes")  # Informa o número de bytes lidos
+                print("Lidos: ", len(data), "Bytes")
                 recebido += len(data)
             print(f"O arquivo '{arquivo}' foi recebido com sucesso.")
             
     else:
         print("Erro: Arquivo não encontrado.")
 
-# Loop para iterar o pedido do arquivo. 
+ 
 while True:
 
     print(f"Conectado ao Servidor: {SERVER}:{PORT}\n")
-    nomeArq = input("Digite o comando desejado: \n\n list: Lista os arquivos no servidor.\n sget <arquivo>: Faz o download de um único arquivo.\n mget <máscara>: Faz o download de múltiplos arquivos utilizando uma máscara.\n hash <arquivo>: Calcula o hash de um arquivo.\n 0: Encerra o programa.\n\nComando: ") # Input para receber o nome do arquivo.
-    if nomeArq == "0": # Caso seja digitado 0, a conexão com o servidor é fechado e o loop é encerrado.
+    nomeArq = input("Digite o comando desejado: \n\n list: Lista os arquivos no servidor.\n sget -> <arquivo>: Faz o download de um único arquivo.\n mget -> <máscara>: Faz o download de múltiplos arquivos utilizando uma máscara.\n cget -> <arquivo>: Continua o download de onde parou em caso de desconexão.\n hash -> <arquivo>: Calcula o hash de um arquivo.\n 0: Encerra o programa.\n\nComando: ") # Input para receber o nome do arquivo.
+    if nomeArq == "0":
         tcpSock.close()
         print("Encerrando programa...")
-        tcpSock.close()
         break
     
     elif nomeArq == "list":
         pedido = "1"
         print("Enviando pedido de listagem dos arquivos.\n")
-        tcpSock.send(pedido.encode('utf-8')) # Enviará o pedido codificado em UTF-8.
-        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+        tcpSock.send(pedido.encode('utf-8'))
+        dataTam = tcpSock.recv(2048)
         resposta = str(dataTam.decode('utf-8'))
         print(resposta)
         break
@@ -61,39 +60,55 @@ while True:
         pedido = "2"
         print("Enviando pedido de download de um único arquivo ao servidor...\n")
         tcpSock.send(pedido.encode('utf-8'))
-        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
-        resposta = str(dataTam.decode('utf-8'))
         
-        
+        dataTam = tcpSock.recv(2048)
+        resposta = (dataTam.decode('utf-8'))
         arquivo = input(resposta)
-        print ("Enviando pedido a", (SERVER, PORT), "para", arquivo) # Caso seja pedido um arquivo, será printado o nome em si ao servidor conectado.
-        tcpSock.send(arquivo.encode('utf-8')) # Enviará o pedido codificado em UTF-8.
+    
+        if arquivo[0:2] == "..": 
+            print ("Enviando pedido a", (SERVER, PORT), "para", arquivo)
+            tcpSock.send(arquivo.encode('utf-8'))
+            dataTam = tcpSock.recv(2048)
+            resposta = (dataTam.decode('utf-8'))
+            print(resposta)
 
-        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+        else:
+            print ("Enviando pedido a", (SERVER, PORT), "para", arquivo)
+            tcpSock.send(arquivo.encode('utf-8'))
 
-        try:
-            tamArq = int(dataTam.decode('utf-8')) # Transforma o pacote contendo o tamanho em inteiro e printa o nome e tamanho.
-            print(f"O arquivo '{arquivo}' possui o tamanho de {tamArq} Bytes.")
-        except ValueError: # Caso o valor recebido seja 0, dará essa exceção.
-            continue
+            dataTam = tcpSock.recv(2048)
 
-        pedir_arquivo(arquivo, tamArq)
+            try:
+                tamArq = int(dataTam.decode('utf-8'))
+                print(f"O arquivo '{arquivo}' possui o tamanho de {tamArq} Bytes.")
+            except ValueError:
+                continue
+
+            pedir_arquivo(arquivo, tamArq)
 
     elif nomeArq == "mget":
         pedido = "3"
         print("Enviando pedido de download de múltiplos arquivos por máscara ao servidor...\n")
         tcpSock.send(pedido.encode('utf-8'))
-        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
-        resposta = str(dataTam.decode('utf-8'))
-        
+        dataTam = tcpSock.recv(2048)
+        resposta = (dataTam.decode('utf-8'))
         
         arquivo = input(resposta)
 
         if arquivo[0:2] != "*.":
             print("Erro de sintaxe, digite um nome válido. Ex.: *.jpg, *.xml.")
+
+        elif arquivo[0:2] == "..":
+            print("Enviando pedido a", (SERVER, PORT), "para todos os arquivos contendo", arquivo)
+            tcpSock.send(arquivo.encode('utf-8'))
+
+            dataTam = tcpSock.recv(2048)
+            resposta = (dataTam.decode('utf-8'))
+            print(resposta)
+            
         else:
-            print("Enviando pedido a", (SERVER, PORT), "para todos os arquivos contendo", arquivo) # Caso seja pedido um arquivo, será printado o nome em si ao servidor conectado.
-            tcpSock.send(arquivo.encode('utf-8')) # Enviará o pedido codificado em UTF-8.
+            print("Enviando pedido a", (SERVER, PORT), "para todos os arquivos contendo", arquivo)
+            tcpSock.send(arquivo.encode('utf-8'))
 
             dataTam = tcpSock.recv(2048)
             resposta = str(dataTam.decode('utf-8'))
@@ -101,14 +116,14 @@ while True:
 
             while True:   
                  
-                dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+                dataTam = tcpSock.recv(2048)
                 print('teste: ', dataTam)
             
                 if dataTam == b'Encerrado':
                     print(f'Todos os arquivos foram recebidos com sucesso!')
                     break
                 else:
-                    dadosArq = dataTam.decode('utf-8') # Transforma o pacote contendo o tamanho em inteiro e printa o nome e tamanho.
+                    dadosArq = dataTam.decode('utf-8')
                     print(dadosArq)
                     arquivo, tamArq = dadosArq.split(':')
                     tamArq = int(tamArq)
@@ -121,32 +136,45 @@ while True:
         print("Enviando pedido de Cálculo de Hash...\n")
         tcpSock.send(pedido.encode('utf-8'))
 
-        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+        dataTam = tcpSock.recv(2048)
         resposta = dataTam.decode('utf-8')
 
         arquivo = input(resposta)
-        tcpSock.send(arquivo.encode('utf-8'))
 
-        hashCalc = tcpSock.recv(2048).decode('utf-8')
-        print(hashCalc)
-        
-        dataTam = tcpSock.recv(2048)
-        resposta2 = dataTam.decode('utf-8')
-        print(resposta2)
-        break
+        if arquivo[0:2] == "..":
+            tcpSock.send(arquivo.encode('utf-8'))
+            dataTam = tcpSock.recv(2048)
+            resposta = (dataTam.decode('utf-8'))
+            print(resposta)
 
-    elif nomeArq== "eget":
+        else:
+            hashCalc = tcpSock.recv(2048).decode('utf-8')
+            print(hashCalc)
+            
+            dataTam = tcpSock.recv(2048)
+            resposta2 = dataTam.decode('utf-8')
+            print(resposta2)
+            break
+
+    elif nomeArq== "cget":
         pedido = "5"
         print("Enviando pedido de continuar o download de um arquivo...\n")
         tcpSock.send(pedido.encode('utf-8'))
 
-        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+        dataTam = tcpSock.recv(2048)
         resposta = dataTam.decode('utf-8')
 
         arquivo = input(resposta)
-        if not ":" in arquivo:
+
+        if arquivo[0:2] == "..":
+            tcpSock.send(arquivo.encode('utf-8'))
+            dataTam = tcpSock.recv(2048)
+            resposta = (dataTam.decode('utf-8'))
+            print(resposta)
+
+        elif not ":" in arquivo:
             tcpSock.send(arquivo.encode)
-            dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+            dataTam = tcpSock.recv(2048)
             resposta = dataTam.decode('utf-8')
             print(resposta)
 
@@ -164,7 +192,7 @@ while True:
                         print(client_hash)
                         tcpSock.send(f"{arquivo}:{client_hash}".encode())
                         
-                    dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+                    dataTam = tcpSock.recv(2048)
                     resposta = dataTam.decode('utf-8')
                     print(resposta)
 
@@ -177,28 +205,27 @@ while True:
                             print(arqLocal)
                             tcpSock.send(f"{int(arqLocal)}".encode())
 
-                        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+                        dataTam = tcpSock.recv(2048)
                         print('Teste',dataTam)
-                        tamArq = int(dataTam.decode('utf-8')) # Transforma o pacote contendo o tamanho em inteiro e printa o nome e tamanho.
+                        tamArq = int(dataTam.decode('utf-8'))
                         print(tamArq)
-                        if tamArq > 0:  # Se o tamanho do arquivo for maior que 0, criaremos e salvaremos o arquivo
+                        if tamArq > 0:
                             
                             with open(DIRETORIO + arquivo, "ab") as fd:
                                 recebido = 0
                                 while recebido < tamArq:
-                                    data = tcpSock.recv(2048)  # Recebe dados em blocos
+                                    data = tcpSock.recv(4096)
                                     fd.write(data)
-                                    print("Lidos: ", len(data), "Bytes")  # Informa o número de bytes lidos
+                                    print("Lidos: ", len(data), "Bytes")  
                                     recebido += len(data)
-                                    print(f"O arquivo '{arquivo}' foi recebido com sucesso.")
-                                dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
-                                resposta = dataTam.decode('utf-8')
-                                print(resposta)
+                            dataTam = tcpSock.recv(2048) 
+                            resposta = dataTam.decode('utf-8')
+                            print(resposta)
 
                     else:
-                        dataTam = tcpSock.recv(2048) # Pacote contendo o tamanho do arquivo solicitado.
+                        dataTam = tcpSock.recv(2048) 
                         resposta = dataTam.decode('utf-8')
                         print(resposta)
-            
 
-            
+    else:
+        print("Comando inválido, digite um comando corretamente.")
